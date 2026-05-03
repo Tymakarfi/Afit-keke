d31da0c55f9a9d360dae03bc1a475b4bde0d1049
Tymakarfi <157685712+Tymakarfi@users.noreply.github.com>
2026-05-03 16:52:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -303,6 +303,7 @@ waiting  = db.execute(“SELECT id, booking_id, name, pickup, destination FROM b
 init_db()
 threading.Thread(target=process_queue, daemon=True).start()
 
-if **name** == ‘**main**’:
-print(“🛺 AFIT Smart-Keke running at http://localhost:5000”)
-app.run(host=‘0.0.0.0’, port=5000, debug=False)
+if __name__ == ‘__main__’:
+    app.run(debug=True)
+
+
